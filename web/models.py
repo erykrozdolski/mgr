b3c2db490d032fabd5e115f9c65a4cfdc8782dde
Eryk Rozdolski <erykrozdolski@gmail.com>
2019-12-11 22:59:00 +0100

    first commit

---
+++ web/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from datetime import datetime
+
+class Responder(models.Model):
+    poll = models.CharField(max_length=200, null=True, blank=True)
+    age = models.CharField(max_length=200, null=True, blank=True)
+    sex = models.CharField(max_length=200, null=True, blank=True)
+    education = models.CharField(max_length=200, null=True, blank=True)
+    living_place = models.CharField(max_length=200, null=True, blank=True)
+    origin_place = models.CharField(max_length=200, null=True, blank=True )
+    martial_status = models.CharField(max_length=200, null=True, blank=True )
+    profession = models.CharField(max_length=200, null=True, blank=True )
+
+
+class Answer(models.Model):
+    value = models.CharField(max_length=200, null=True, blank=True, default="no answer")
+    power = models.CharField(max_length=200, null=True, blank=True, default="no answer")
+    category = models.CharField(max_length=250, null=True, blank=True, default="no answer")
+    responder = models.ManyToManyField("Responder", related_name="answer_responder")
+
+class BigAnswer(models.Model):
+    answers = models.ManyToManyField("Answer", related_name='answers')
+    power = models.CharField(max_length=200, null=True, blank=True, default="no answer")
+    category = models.CharField(max_length=250, null=True, blank=True, default="no answer")
+    responder = models.ManyToManyField("Responder", related_name="big_answer_responder")
+
+
+class FirstPoll(models.Model):
+    offensive_words = models.ManyToManyField("Answer", related_name='_s_offensive_words')
+    free_time = models.ManyToManyField("Answer", related_name='_s_free_time')
+    movie = models.ManyToManyField("Answer", related_name='_s_movie')
+    what_you_like = models.ManyToManyField("Answer", related_name='_s_what_you_like')
+    important_in_life = models.ManyToManyField("Answer", related_name='_s_important_in_life')
+    music = models.ManyToManyField("Answer", related_name='_s_music')
+
+
+class SecondPoll(models.Model):
+    kurwa = models.ManyToManyField("BigAnswer", related_name='_s_kurwa')
+    chuj = models.ManyToManyField("BigAnswer", related_name='_s_chuj')
+    debil = models.ManyToManyField("BigAnswer", related_name='_s_debil')
+    idiota = models.ManyToManyField("BigAnswer", related_name='_s_idiota')
+    szmata = models.ManyToManyField("BigAnswer", related_name='_s_szmata')
+    pizda = models.ManyToManyField("BigAnswer", related_name='_s_pizda')
+    skurwysyn = models.ManyToManyField("BigAnswer", related_name='_s_skurwysyn')
+    dziwka = models.ManyToManyField("BigAnswer", related_name='_s_dziwka')
---
+++ web/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+import migrate_to_db
+# Create your views here.
